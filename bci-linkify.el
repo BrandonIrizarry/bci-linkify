839291d71755a0c3359ef07a0b68352a5af5bfda
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-03-23 17:38:10 -0400

    Update to newer version of linkification
    This one, like my original version, works solely with Org headers,
    which is my most common use case so far anyway.

--- bci-linkify.el
+++ bci-linkify.el
@@ -1,36 +1,14 @@
 ; -*- lexical-binding: t -*-
 
-(defun bci/--prelinkify ()
-  "Format the relevant text using the Org link format.
-
-Another function inserts the link destination."
-  (pcase-let ((`(,r1 . ,r2) (if (use-region-p)
-                                (car (region-bounds))
-                              (bounds-of-thing-at-point 'word))))
-    (let ((text (buffer-substring-no-properties r1 r2)))
-      (delete-region r1 r2)        ; Get rid of the original text
-      (insert (with-temp-buffer    ; Create and insert the link format
-                (insert text)
-                (beginning-of-buffer)
-                (insert "[[][")
-                (end-of-buffer)
-                (insert "]]")
-                (buffer-string))))))
-
-(defun bci/linkify (link)
-  "Linkify the current region.
-
-If no region, use the current word."
-  (interactive "MLink: ")
+(defun bci/linkify-header-content (link-destination)
+  "Linkify Org header text."
+  (interactive "MDestination: ")
   (save-excursion
-    (bci/--prelinkify)
-
-    ;;Note: `bci/--prelinkify' assumes we've ended up at the far-right
-    ;;of the formatted text. Now we have to move backward to insert
-    ;;the link.
-    (re-search-backward "\\[]")
-
-    (forward-char)
-    (insert link)))
-
-(bind-key "C-c l" #'bci/linkify org-mode-map)
+    (beginning-of-line)
+    (when (looking-at org-heading-regexp)
+      (let ((data (nthcdr 4 (match-data 'integers))))
+        (seq-let (content-start content-end) data
+          (let ((content (buffer-substring-no-properties content-start content-end)))
+            (goto-char content-start)
+            (delete-region content-start content-end)
+            (org-insert-link nil link-destination content)))))))
