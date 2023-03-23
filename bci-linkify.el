0df254038d570df0f4c3f6df4a71dd19e0006c31
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-03-23 18:19:24 -0400

    Add docstring and comments to function

--- bci-linkify.el
+++ bci-linkify.el
@@ -2,11 +2,18 @@
 
 ;;;###autoload
 (defun bci-linkify-header-content (link-destination)
-  "Linkify Org header text."
+  "Linkify Org header text.
+
+If on a line containing an Org header, make the header text into
+a link pointing to LINK-DESTINATION.
+
+Else, do nothing and return NIL."
   (interactive "MDestination: ")
   (save-excursion
     (beginning-of-line)
     (when (looking-at org-heading-regexp)
+      ;; nthcdr: skip to the header content's match-data
+      ;; See documentation for 'match-data'
       (let ((data (nthcdr 4 (match-data 'integers))))
         (seq-let (content-start content-end) data
           (let ((content (buffer-substring-no-properties content-start content-end)))
